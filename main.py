88aa1c06cdd3be71910f8005385c342f9611aafe
Rika Dewi <rika_4444@yahoo.com>
2020-03-02 03:38:32 +0700

    Solved cors policy

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from flask_cors import CORS
 from label import create_app
 from label.database import db
 import config
 
 if __name__ == '__main__':
-    app = create_app()    
+    app = create_app()   
+    CORS(app) 
     db.create_connection(config.DATABASE_NAME)
     app.run(debug = True)
     db.close_connection()
