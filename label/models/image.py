e57919699eda28d5d72bf1dc82e9a58a84901f88
rika <rika_4444@yahoo.com>
2020-03-25 23:36:09 +0700

    Implement ping image id

--- label/models/image.py
+++ label/models/image.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request, Response
 from sqlite3 import Error
 from label.database import db
 from label.utils import const
+import time
 
 image_bp = Blueprint('image_bp', __name__,
                     template_folder='templates',
@@ -10,40 +11,9 @@ image_bp = Blueprint('image_bp', __name__,
 ID = 0
 STATUS = 1
 FILENAME = 2
+LAST_UPDATE = 3
+
 COUNT_PAGE = 25
-
-# Fetch an image from given id
-@image_bp.route('/image/<id>', methods=['GET'])
-def get_image(id):
-    try:
-        cur = db.conn.cursor()
-        cur.execute("SELECT * FROM images WHERE id_image=?", id)
-        row = cur.fetchone()
-        cur.close()
-
-    except Error as e:
-        return jsonify({"error": "can't fetch image"}), 500
-    
-    return jsonify({
-        "filename": row[FILENAME],
-        "status": row[STATUS]
-    }), 200
-
-# Save image to database as unlabeled
-@image_bp.route('/image', methods=['POST'])
-def post_image():
-    req = request.get_json()
-    
-    try:
-        cur = db.conn.cursor()
-        for filename in req['filenames']:
-            cur.execute("INSERT INTO images (status, filename) VALUES (?, ?);", (const.UNLABELED, filename))
-        db.conn.commit()
-        cur.close()
-    except Error as e:
-        return jsonify({"error": "can't fetch image"}), 500
-
-    return Response(status=200)
 
 # Fetch all image
 @image_bp.route('/image/all/<page>', methods=['GET'])
@@ -80,3 +50,57 @@ def delete_all_image():
     return jsonify({
         "count": count
     }), 200
+
+# Ping image id
+@image_bp.route('/image/ping/<id>', methods=['PUT'])
+def ping_image(id):
+    try:
+        cur = db.conn.cursor()
+        cur.execute("UPDATE images SET last_update=:time WHERE id_image=:id", {"time": time.time(), "id": id})
+        cur.close()
+
+    except Error as e:
+        return jsonify({"error": "can't update last_update in ping image"}), 500
+
+    return jsonify({
+        "id": id
+    }), 200
+
+# Fetch an image from given id
+@image_bp.route('/image/<id>', methods=['GET'])
+def get_image(id):
+    try:
+        cur = db.conn.cursor()
+        print(id)
+        cur.execute("SELECT * FROM images WHERE id_image=:id", {"id": id})
+        row = cur.fetchone()
+        cur.close()
+
+    except Error as e:
+        print(e)
+        return jsonify({"error": "can't fetch one image"}), 500
+    
+    if row == None:
+        return jsonify({"error": "id for image not found"}), 404
+
+    return jsonify({
+        "filename": row[FILENAME],
+        "status": row[STATUS],
+        "last_update": row[LAST_UPDATE]
+    }), 200
+
+# Save image to database as unlabeled
+@image_bp.route('/image', methods=['POST'])
+def post_image():
+    req = request.get_json()
+    
+    try:
+        cur = db.conn.cursor()
+        for filename in req['filenames']:
+            cur.execute("INSERT INTO images (status, filename) VALUES (?, ?);", (const.UNLABELED, filename))
+        db.conn.commit()
+        cur.close()
+    except Error as e:
+        return jsonify({"error": "can't post image"}), 500
+
+    return Response(status=200)
